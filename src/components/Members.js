7c59e8f9d8183c1851cfc92fe40924db294b2387
AM-77 <amine.griche77@gmail.com>
2020-06-03 18:47:17 +0200

    refactore code

--- src/components/Members.js
+++ src/components/Members.js
@@ -2,14 +2,12 @@ import React, { Component } from 'react'
 
 export default class Members extends Component {
     render() {
+        const { room, members, me } = this.props
         return (
             <div className="members-container">
-                <h2 className="title">{this.props.room}'s Members</h2>
-                {
-                    this.props.members.map((member, index) => <h5 key={index} className={`member ${member.name === this.props.me ? "me" : ""}`}><span className="fa fa-user"></span>{member.name}</h5>)
-                }
-                <div className="logout">
-                    <button onClick={this.props.logout}>Logout</button>
+                <h2 className="title">The Members</h2>
+                <div className="members-list">
+                    { members.map((member, index) => <h5 key={index} className={`member ${member.name === me ? "me" : "others"}`}># {member.name}</h5>) }
                 </div>
             </div>
         )
