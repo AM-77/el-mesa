14b8a82a1967de921a24bb47e0d12f337a9d7c24
AM-77 <amine.griche77@gmail.com>
2020-06-03 18:48:37 +0200

    refactore code

--- src/components/Rooms.js
+++ src/components/Rooms.js
@@ -2,13 +2,13 @@ import React, { Component } from 'react'
 
 export default class Rooms extends Component {
     render() {
+        const { rooms, selectRoom } = this.props
         return (
             <div className="rooms-container">
                 <h2>Chat Rooms</h2>
                 {
-                    this.props.rooms.length > 0
-                        ?
-                        this.props.rooms.map((room, index) => <h5 key={index} className="room">{room}</h5>)
+                    rooms.length > 0 ?
+                        rooms.map((room, index) => <h5 onClick={() => selectRoom(room)} key={index} className="room">{room}</h5>)
                         :
                         <h5 className="no-room room">No Chat Room</h5>
                 }
