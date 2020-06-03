1600ba859f044fbacfda6364a6f59115fc0cd6cb
AM-77 <amine.griche77@gmail.com>
2020-06-03 18:48:22 +0200

    refactore code

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,18 +1,20 @@
 import React, { Component } from "react"
+import logo from '../assets/logo.png'
 
 export default class NavBar extends Component {
     render() {
+        const { logout, join, room } = this.props
         return (
-            <div className="nav-bar-container">
+            <div className={`nav-bar-container ${join ? 'join': 'room'}`}>
+                <div className="logo-wrapper">
+                    <div className="logo-image"><img src={logo} alt="The logo" /></div>
+                    <h1 className="logo-title">{room ? `${room}`: 'El Mesa'}</h1>
+                </div>
                 {
-                    <div className="join-nav-bar">
-                        <div className="center">
-                            <div><img src={`${process.env.PUBLIC_URL}/logo.png`} alt="El kara's logo" /></div>
-                            <h1 className="title">El Kara</h1>
-                        </div>
+                    logout && <div className="logout">
+                        <button onClick={logout}>Logout</button>
                     </div>
                 }
-
             </div>
         )
     }
