ccc2650865f81081167c0981a25e2b56f9f29f5c
AM-77 <amine.griche77@gmail.com>
2020-06-10 21:15:18 +0200

    remove fontawesome import

--- src/index.js
+++ src/index.js
@@ -4,8 +4,6 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-import "@fortawesome/fontawesome-free/css/all.min.css"
-
 ReactDOM.render(<App />, document.getElementById('root'));
 
 serviceWorker.unregister();
